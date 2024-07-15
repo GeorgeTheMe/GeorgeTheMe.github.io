075e6836a27865c3d47f9899087c89d43aeee755
georgygorovoy <georgy.gorovoy@gmail.com>
2024-07-15 21:17:58 +0300

    Prompt update!
    Now you can enter your name, and than there are a birthday card for your birthday!

---
+++ happy.js
@@ -0,0 +1,5 @@
+const dear = document.querySelector(".Dear")
+const output = document.querySelector("#output")
+const prompt1 = prompt ("Enter your name, please!")
+dear.textContent = "Dear, "+prompt1
+output.textContent = prompt1 
